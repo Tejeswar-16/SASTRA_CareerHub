b4c662e2a54e7f4e050ca0553ceb447622fb454b
Tejeswar-16 <tejeswarsakthidaran@gmail.com>
2025-09-29 22:34:12 +0530

    fixed colour issue

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -21,14 +21,18 @@ export default function HOME(){
         })
     })
 
+    function handleLogoClick(){
+        router.push("/dashboard");
+    }
+
     return(
         <>
             <div className="relative bg-gray-100 py-5 min-h-screen md:bg-gray-100">
                 <div className="mx-auto bg-white p-2 rounded-xl shadow-xl w-350">
                     <div className="flex flex-row justify-between">
                         <div className="flex flex-row justify-left items-center">
-                            <Image src={"/logo.png"} width={50} height={20} alt="Logo"></Image>
-                            <div className="font-sans font-bold text-2xl">Welcome, {username}</div>
+                            <Image onClick={handleLogoClick} className="hover:cursor-pointer" src={"/logo.png"} width={60} height={20} alt="Logo"></Image>
+                            <div className="select-none font-sans font-bold text-3xl">Welcome, {username}</div>
                         </div>
                         <div className="flex flex-row justify-between items-center space-x-6">
                             <div className="font-sans text-xl">Resource Library</div>
@@ -40,29 +44,29 @@ export default function HOME(){
                 <div className="flex flex-row">
                     <div className="ml-18 my-20 p-5 bg-white rounded-2xl shadow-gray-500 shadow-2xl w-80 hover:scale-105 transition duration-300">
                         <Image className="mx-auto rounded-xl" src={"/resources.jpg"} width={360} height={360} alt="resources"></Image>
-                        <div className="text-center font-sans mt-2 font-bold text-2xl">Resource Library</div>
-                        <div className="text-center italic font-sans text-lg">Boost your career with expert resources on DSA, aptitude, interviews, HR, and more. Learn, practice, and excel!</div>
+                        <div className="select-none text-center font-sans mt-2 font-bold text-2xl">Resource Library</div>
+                        <div className="select-none text-center italic font-sans text-lg">Boost your career with expert resources on DSA, aptitude, interviews, HR, and more. Learn, practice, and excel!</div>
                         <Image className="mx-auto  hover:cursor-pointer" src={"/navigate.jpg"} width={80} height={10} alt="navigate"></Image>
                     </div>
 
                     <div className="ml-10 my-20 p-5 bg-white rounded-2xl shadow-gray-500 shadow-2xl w-80 hover:scale-105 transition duration-300">
                         <Image className="mx-auto rounded-xl" src={"/company.jpg"} width={360} height={360} alt="resources"></Image>
-                        <div className="text-center font-sans mt-2 font-bold text-2xl">Comapany Tracker</div>
-                        <div className="text-center italic font-sans text-lg">Stay informed about SASTRA placements, internship openings, and company updates to plan your future</div>
+                        <div className="select-none text-center font-sans mt-2 font-bold text-2xl">Comapany Tracker</div>
+                        <div className="select-none text-center italic font-sans text-lg">Stay informed about SASTRA placements, internship openings, and company updates to plan your future</div>
                         <Image className="mx-auto  hover:cursor-pointer" src={"/navigate.jpg"} width={80} height={10} alt="navigate"></Image>
                     </div>
 
                     <div className="ml-10 my-20 p-5 bg-white rounded-2xl shadow-gray-500 shadow-2xl w-80 hover:scale-105 transition duration-300">
                         <Image className="mx-auto rounded-xl" src={"/progress.jpg"} width={360} height={360} alt="resources"></Image>
-                        <div className="text-center font-sans mt-2 font-bold text-2xl">Track your progress</div>
-                        <div className="text-center italic font-sans text-lg">Discover your strengths, identify areas to improve, and keep moving forward. Every step counts!</div>
+                        <div className="select-none text-center font-sans mt-2 font-bold text-2xl">Track your progress</div>
+                        <div className="select-none text-center italic font-sans text-lg">Discover your strengths, identify areas to improve, and keep moving forward. Every step counts!</div>
                         <Image className="mx-auto  hover:cursor-pointer" src={"/navigate.jpg"} width={80} height={10} alt="navigate"></Image>
                     </div>
 
                     <div className="ml-10 mr-18 my-20 p-5 bg-white rounded-2xl shadow-gray-500 shadow-2xl w-80 hover:scale-105 transition duration-300">
                         <Image className="mx-auto rounded-xl" src={"/interview.jpg"} width={360} height={360} alt="resources"></Image>
-                        <div className="text-center font-sans mt-2 font-bold text-2xl">Interview Experience</div>
-                        <div className="text-center italic font-sans text-lg">Your story can guide someone&apos;s journey. Share your interview experience and discover others&apos; too.</div>
+                        <div className="select-none text-center font-sans mt-2 font-bold text-2xl">Interview Experience</div>
+                        <div className="select-none text-center italic font-sans text-lg">Your story can guide someone&apos;s journey. Share your interview experience and discover others&apos; too.</div>
                         <Image className="mx-auto  hover:cursor-pointer" src={"/navigate.jpg"} width={80} height={10} alt="navigate"></Image>
                     </div>
                 </div>
