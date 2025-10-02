a7cdcc9c6da83e078be9161c2612a6a62bc9bb1e
Tejeswar-16 <tejeswarsakthidaran@gmail.com>
2025-10-02 23:42:47 +0530

    added dynamic topic page

--- src/app/[registerNumber]/resources/[topic]/page.js
+++ src/app/[registerNumber]/resources/[topic]/page.js
@@ -3,6 +3,7 @@
 import { auth } from "@/app/_util/config";
 import NavBar from "@/app/NavBar"
 import { onAuthStateChanged } from "firebase/auth";
+import { useParams } from "next/navigation";
 import { useEffect, useState } from "react"
 
 export default function Home(){
@@ -10,6 +11,8 @@ export default function Home(){
     const [username,setUsername] = useState("");
     const [email,setEmail] = useState("");
     const [darkMode,setDarkMode] = useState(false);
+
+    const params = useParams();
 
     useEffect(() => {
         onAuthStateChanged(auth,(user) => {
@@ -47,10 +50,27 @@ export default function Home(){
         document.documentElement.classList.remove('dark')
     }
 
+    const topicMap = {
+        "aptitude" : "Aptitude",
+        "arrays" : "Arrays",
+        "strings" : "Strings",
+        "dynamicprogramming" : "Dynamic Programming (DP)",
+        "graphs" : "Graphs",
+        "trees" : "Trees",
+        "operatingsystems" : "Operating Systems (OS)",
+        "computernetworks" : "Computer Networks (CN)",
+        "objectorientedprogramming" : "Object Oriented Programming (OOPs)",
+        "dbms" : "Database Management System (DBMS)",
+        "hr" : "Human Resource (HR)"
+    }
+
     return (
         <>
             <div className="relative bg-gray-100 py-5 min-h-screen md:bg-gray-100">
                 <NavBar username={username} email={email} darkMode={darkMode} handleLogoClick={handleLogoClick} handleDarkMode={handleDarkMode} handleLightMode={handleLightMode} handleLogout={handleLogout}></NavBar>
+                <div className="mx-auto bg-white rounded-xl shadow-lg my-5 p-2 w-75 md:w-190 lg:w-250">
+                    <div className="flex justify-center font-sans font-bold text-2xl">{topicMap[params.topic]}</div>
+                </div>
             </div>
         </>
     )
