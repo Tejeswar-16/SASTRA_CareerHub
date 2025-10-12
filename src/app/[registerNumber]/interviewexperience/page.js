4cf52750a2c049b1a7b22c16eee89b6d17d9d2d5
Tejeswar-16 <tejeswarsakthidaran@gmail.com>
2025-10-12 22:37:26 +0530

    implemented firebase for interview page

--- src/app/[registerNumber]/interviewexperience/page.js
+++ src/app/[registerNumber]/interviewexperience/page.js
@@ -1,8 +1,9 @@
 "use client"
 
-import { auth } from "@/app/_util/config";
+import { auth, db } from "@/app/_util/config";
 import NavBar from "@/app/NavBar"
 import { onAuthStateChanged, signOut } from "firebase/auth";
+import { addDoc, collection, getDocs, orderBy, query } from "firebase/firestore";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react"
@@ -11,10 +12,13 @@ export default function Home(){
 
     const [username,setUsername] = useState("");
     const [email,setEmail] = useState("");
-    const [emoji,setEmoji] = useState(false);
-    const [emojiClick,setEmojiClick] = useState([]);
-    const [emojiCount,setEmojiCount] = useState([]);
     const [addPost,setAddPost] = useState(false);
+    const [postContent,setPostContent] = useState("");
+    const [alert,setAlert] = useState(false);
+    const [loading,setLoading] = useState(false);
+    const [postData,setPostData] = useState([]);
+    const [emoji,setEmoji] = useState([]);
+    const [emojiClick,setEmojiClick] = useState([]);
 
     const router = useRouter();
 
@@ -43,6 +47,84 @@ export default function Home(){
         }
     }
 
+    useEffect(() => {
+        if (postData.length > 0){
+            setEmoji(new Array(postData.length).fill(false));
+            setEmojiClick(new Array(postData.length).fill(null));
+        }
+    },[postData]);
+
+    function getRegNo(){
+        return email.substring(0,9);
+    }
+
+    async function handleAddPost(){
+        try
+        {
+            setLoading(true);
+            await addDoc(collection(db,"posts"),{
+                post: postContent,
+                addedAt: new Date(),
+                regno: getRegNo()
+            });
+            setLoading(false);
+            setAddPost(false);
+            setAlert(true);
+        }
+        catch(error)
+        {
+            console.log(error.message);
+        }
+    }
+
+    function handleAlertOk(){
+        setAlert(false);
+    }
+
+    async function fetchPosts(){
+        try
+        {
+            setLoading(true);
+            const q = query(
+                collection(db,"posts"),
+                orderBy("addedAt","desc")
+            );
+            const querySnapshot = await getDocs(q);
+            const data = querySnapshot.docs.map((doc) => doc.data());
+            setPostData(data);
+            setLoading(false);
+        }
+        catch(error)
+        {
+            console.log(error.message);
+        }
+    }
+
+    useEffect(() => {
+        fetchPosts();
+    },[]);
+
+    function handleEmojiClick(index){
+        setEmoji((prev) => {
+            const newArr = [...prev];
+            newArr[index] = true;
+            return newArr;
+        });
+    }
+
+    function selectEmoji(index,emojiChar){
+        setEmojiClick((prev) => {
+            const updated = [...prev];
+            updated[index] = emojiChar;
+            return updated;
+        });
+        setEmoji((prev) => {
+            const updated = [...prev];
+            updated[index]  = false;
+            return updated;
+        })
+    }
+
     return (
         <>
             <div className="relative bg-gray-100 py-5 min-h-screen md:bg-gray-100">
@@ -68,68 +150,91 @@ export default function Home(){
                                         <div className="select-none font-sans font-bold text-2xl">Share your experience</div>
                                         
                                     </div>
-                                    <textarea placeholder="Share your thoughts..." className="resize-none h-70 w-80 p-2 my-2 text-lg rounded-xl border"></textarea>
-                                    <button className="select-none font-sans font-semibold text-white p-2 rounded-xl shadoe-xl bg-blue-500 hover:bg-blue-700 hover:cursor-pointer transition duration-300">Add Post</button>
+                                    <textarea onChange={(e) => setPostContent(e.target.value)} placeholder="Share your thoughts..." className="resize-none h-70 w-80 p-2 my-2 text-lg rounded-xl border"></textarea>
+                                    <button onClick={handleAddPost} className="select-none font-sans font-semibold text-white p-2 rounded-xl shadoe-xl bg-blue-500 hover:bg-blue-700 hover:cursor-pointer transition duration-300">Add Post</button>
                                 </div>
                             </div>
                         </div>
                 }
 
-                <div className="mx-auto w-250 bg-gray-300 border-b-5 border-t-5 border-l-2 border-r-2 rounded-xl mt-5 h-140 overflow-hidden overflow-y-auto">
-                    <div className="mx-auto bg-white w-230 rounded-xl shadow-xl p-2 my-5 border border-t-5 border-b-5 border-r-3 border-l-3 border-blue-900">
-                        <div className="flex flex-row justify-between items-center px-18 py-2">
-                            <div className="flex flex-row items-center gap-x-2">
-                                <div className="select-none bg-gray-300 font-sans font-bold text-2xl text-black rounded-3xl border-b-2 border-r-2 border p-2">
-                                    {username[0]}
-                                </div>
-                                <div className="flex flex-col">
-                                    <div className="select-none font-sans font-semibold">
-                                        {username}
-                                    </div>
-                                    <div className="select-none font-sans font-semibold">
-                                        {email}
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="flex flex-col">
-                                <div className="select-none font-sans font-semibold">Posted on</div>
-                                <div className="select-none font-sans font-semibold">11th Oct, 2025</div>
-                            </div>
-                        </div>
-                        <hr className="mx-auto my-2 w-190 text-gray-500 font-bold"></hr>
-                        <div className="select-none mt-2 mx-auto font-sans w-190">
-                            lorem ipsum lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum
-                        </div>
-                        <hr className="mx-auto my-2 w-190 text-gray-500 font-bold"></hr>
-                        <div className="flex flex-row">
-                            {
-                                !emoji && 
-                                    <div className="select-none ml-15 text-2xl font-sans w-190">
-                                        <Image onClick={() => setEmoji(true)} className="hover:cursor-pointer hover:scale-150 transition duration-300"src={"/like.png"} width={50} height={20} alt="like"></Image>
-                                    </div>
-                            }
-                            {
-                                emoji && 
-                                    <div className="flex flex-row jusify-between select-none ml-17 rounded-xl shadow-2xl mt-2 w-50 text-2xl font-sans">
-                                        <div onClick={() => {setEmojiClick([true,false,false,false,false,false,false]);setEmoji(false)}} className="hover:cursor-pointer hover:scale-150 transition duration-300">👍</div>
-                                        <div onClick={() => {setEmojiClick([false,true,false,false,false,false,false]);setEmoji(false)}} className="hover:cursor-pointer hover:scale-150 transition duration-300">❤️</div>
-                                        <div onClick={() => {setEmojiClick([false,false,true,false,false,false,false]);setEmoji(false)}} className="hover:cursor-pointer hover:scale-150 transition duration-300">🙏</div>
-                                        <div onClick={() => {setEmojiClick([false,false,false,true,false,false,false]);setEmoji(false)}} className="hover:cursor-pointer hover:scale-150 transition duration-300">👏</div>
-                                        <div onClick={() => {setEmojiClick([false,false,false,false,true,false,false]);setEmoji(false)}} className="hover:cursor-pointer hover:scale-150 transition duration-300">😂</div>
-                                        <div onClick={() => {setEmojiClick([false,false,false,false,false,true,false]);setEmoji(false)}} className="hover:cursor-pointer hover:scale-150 transition duration-300">😊</div>
-                                        <div onClick={() => {setEmojiClick([false,false,false,false,false,false,true]);setEmoji(false)}} className="hover:cursor-pointer hover:scale-150 transition duration-300">😲</div>
-                                    </div>
-                            }
-                            {   emojiClick[0] && <div className="flex flex-row"><div className="select-none flex justify-end text-xl">👍</div><div className="font-sans mr-16 text-lg">{emojiCount[0]}</div></div> }
-                            {   emojiClick[1] && <div className="flex flex-row"><div className="select-none flex justify-end text-xl">❤️</div><div className="font-sans mr-16 text-lg">{emojiCount[1]}</div></div> }
-                            {   emojiClick[2] && <div className="flex flex-row"><div className="select-none flex justify-end text-xl">🙏</div><div className="font-sans mr-16 text-lg">{emojiCount[2]}</div></div> }
-                            {   emojiClick[3] && <div className="flex flex-row"><div className="select-none flex justify-end text-xl">👏</div><div className="font-sans mr-16 text-lg">{emojiCount[3]}</div></div> }
-                            {   emojiClick[4] && <div className="flex flex-row"><div className="select-none flex justify-end text-xl">😂</div><div className="font-sans mr-16 text-lg">{emojiCount[4]}</div></div> }
-                            {   emojiClick[5] && <div className="flex flex-row"><div className="select-none flex justify-end text-xl">😊</div><div className="font-sans mr-16 text-lg">{emojiCount[5]}</div></div> }
-                            {   emojiClick[6] && <div className="flex flex-row"><div className="select-none flex justify-end text-xl">😲</div><div className="font-sans mr-16 text-lg">{emojiCount[6]}</div></div> }
+                {
+                    alert && 
+                    <div className="fixed inset-0 flex flex-col justify-center backdrop-blur-sm items-center">
+                        <div className="select-none font-sans bg-white rounded-xl shadow-xl p-5 border">
+                        <p className="text-lg">Post added successfully!</p>
+                        <div className="flex justify-center "><button onClick={handleAlertOk} className="bg-black text-white w-10 rounded-xl mt-2 p-2 hover:cursor-pointer">OK</button></div>
                         </div>
                     </div>
-                    
+                }
+
+                {loading && 
+                    <>
+                        <div className="fixed inset-0 flex flex-col justify-center backdrop-blur-sm items-center">
+                            <div className="mx-auto font-mono font-bold text-3xl">
+                                <Image src={"/loading.gif"} width={200} height={20} alt="Loading..."></Image>
+                            </div>
+                        </div>
+                    </>
+                }
+
+                <div className="mx-auto w-250 bg-gray-300 border-b-5 border-t-5 border-l-2 border-r-2 rounded-xl mt-5 h-140 overflow-hidden overflow-y-auto">
+                    {
+                        postData.map((post,index) => (
+                            <div key={index}>
+                                <div className="mx-auto bg-white w-230 rounded-xl shadow-xl p-2 my-5 border border-t-5 border-b-5 border-r-3 border-l-3 border-blue-900">
+                                    <div className="flex flex-row justify-between items-center px-18 py-2">
+                                        <div className="flex flex-row items-center gap-x-2">
+                                            <div className="select-none bg-gray-300 font-sans font-bold text-2xl text-black rounded-3xl border-b-2 border-r-2 border p-2">
+                                                {username[0]}
+                                            </div>
+                                            <div className="flex flex-col">
+                                                <div className="select-none font-sans font-semibold">
+                                                    {username}
+                                                </div>
+                                                <div className="select-none font-sans font-semibold">
+                                                    {email}
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="flex flex-col">
+                                            <div className="select-none font-sans font-semibold">Posted on</div>
+                                            <div className="select-none font-sans font-semibold">{post.addedAt.toDate().toLocaleString()}</div>
+                                        </div>
+                                    </div>
+                                    <div className="select-none my-2 rounded-xl shadow-xl shadow-gray-200 border p-2 mx-auto font-sans w-190">
+                                        {post.post}
+                                    </div>
+                                    <div className="flex flex-row">
+                                        {
+                                            !emoji[index] && 
+                                                <div className="select-none ml-15 text-2xl font-sans w-190">
+                                                    <Image onClick={() => handleEmojiClick(index)} className="hover:cursor-pointer hover:scale-150 transition duration-300"src={"/like.png"} width={50} height={20} alt="like"></Image>
+                                                </div>
+                                        }
+                                        {
+                                            emoji[index] && 
+                                                <div className="flex flex-row jusify-between select-none ml-17 rounded-xl shadow-2xl mt-2 w-50 text-2xl font-sans">
+                                                    {
+                                                        ["👍","❤️","🙏","👏","😂","😊","😲"].map((emojiChar,eIndex) => (
+                                                            <div key={eIndex} onClick={() => selectEmoji(index,emojiChar)} className="hover:cursor-pointer hover:scale-150 transition duration-300">
+                                                                {emojiChar}
+                                                            </div>
+                                                        ))
+                                                    }
+                                                </div>
+                                        }
+                                        {
+                                            emojiClick[index] && (
+                                                <div className="flex flex-row mr-17 text-xl">
+                                                    {emojiClick[index]}
+                                                </div>
+                                            )
+                                        }
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
         </>
