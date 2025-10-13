03a2a37820ddd7cecd25e005b4eb32ce0e4e01a3
Tejeswar-16 <tejeswarsakthidaran@gmail.com>
2025-10-13 23:40:40 +0530

    updated company tracker

--- src/app/[registerNumber]/companytracker/page.js
+++ src/app/[registerNumber]/companytracker/page.js
@@ -12,6 +12,7 @@ export default function Home(){
 
     const [name,setName] = useState("");
     const [email,setEmail] = useState("");
+    const [idx,setIdx] = useState(0);
     const [loading,setLoading] = useState(false);
     const [companyData,setCompanyData] = useState([]);
 
@@ -70,6 +71,23 @@ export default function Home(){
         router.push("/"+params.registerNumber+"/companytracker/"+Company.toLowerCase());
     }
 
+    const colors = [
+        "bg-blue-200",
+        "bg-violet-200",
+        "bg-green-200",
+        "bg-yellow-200",
+        "bg-purple-200",
+        "bg-indigo-200",
+        "bg-red-200",
+        "bg-fuchsia-200"
+    ];
+
+    useEffect(() => {
+        setInterval(() => {
+            setIdx((prev => (prev+1)%colors.length));
+        },1000);
+    },[]);
+
     return (
         <>
             <div className="relative bg-gray-100 py-5 min-h-screen md:bg-gray-100">
@@ -83,32 +101,40 @@ export default function Home(){
                             </div>
                         </>
                     }
-                    <div className="mx-auto flex flex-col md:flex-wrap md:flex-row justify-center md:gap-9 w-75 md:w-190 lg:w-250"> 
+                    <div className="mx-auto flex flex-col md:flex-wrap md:flex-row justify-between w-75 md:w-190 lg:w-250"> 
                         {
                             companyData.map((company,index) => (
                                 <div key={index}>
-                                    <div className="select-none border border-b-6 border-r-3 border-l-2 border-blue-900 hover:scale-105 flex flex-col items-center bg-white rounded-xl shadow-xl mt-10 w-75 md:w-90 lg:w-120 transition duration-300">
+                                    <div className="select-none border border-b-6 border-r-3 border-l-2 border-blue-900 hover:scale-105 flex flex-col items-center bg-white rounded-xl shadow-xl mt-5 w-75 md:w-90 lg:w-120 transition duration-300">
                                         <div className="flex justify-center font-sans rounded-lg bg-blue-900 text-white p-2 text-md lg:text-xl w-74 md:w-89 lg:w-119 font-bold">
-                                            {company.title}
+                                            {company.type}
                                         </div>
                                         <div className="select-none p-5">
-                                            <div className="flex flex-row justify-between items-center">
-                                                <div className="flex flex-col lg:mr-27">
-                                                    <div className="font-sans text-sm md:text-md font-semibold">
+                                            <div className="flex flex-col justify-between items-center">
+                                                <div className="flex flex-row justify-between gap-x-5 items-center">
+                                                    <div className={`font-sans flex justify-center md:ml-4 ${company.status === "Active" ? "bg-green-700" : "bg-red-700"} p-2 text-sm rounded-xl text-white`}>
+                                                        {company.status}
+                                                    </div>
+                                                    <div className={`font-sans font-bold text-sm ${colors[idx]} rounded-xl p-2 transition duration-300`}>
+                                                        Deadline: {company.deadline.toDate().toLocaleString()}
+                                                    </div>
+                                                </div>
+                                                <div className="flex flex-col mt-5">
+                                                    <div className="font-sans font-semibold">
                                                         Role: {company.role}
                                                     </div>
-                                                    <div className="font-sans text-sm lg:text-md font-semibold">
+                                                    <div className="font-sans font-semibold">
                                                         Batch(es) Eligible: {company.batch.join(", ")} passouts
                                                     </div>
-                                                    <div className="font-sans text-sm md:text-md font-semibold">
+                                                    <div className="font-sans font-semibold">
                                                         Department: {company.forDept.join(", ")}
                                                     </div>
                                                 </div>
-                                                <div className={`font-sans flex md:ml-4 ${company.status === "Active" ? "bg-green-700" : "bg-red-700"} p-1 text-sm rounded-xl text-white`}>
-                                                    {company.status}
-                                                </div>
                                             </div>
                                             <div className="font-sans text-sm h-48 mt-2 p-2 border border-gray-300 rounded-xl overflow-hidden overflow-y-auto">
+                                                <div  className="select-none font-sans mx-auto font-semibold text-lg">
+                                                    {company.title}
+                                                </div>
                                                 {company.content}
                                             </div>
                                             <div className="flex flex-col items-center md:flex-row justify-between md:space-x-3 mt-5">
